de6ddf275e8cee0628ca1360693090ccf7a95d39
alice <aliceisjustplaying@gmail.com>
2024-09-29 20:11:54 +0200

    fix shutdown logic

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,17 @@ dotenv.config();
 logger.info('Hello, World!');
 
 function shutdown() {
+  setTimeout(() => {
+    logger.error('Forcing shutdown.');
+    process.exit(1);
+  }, 60000);
+
+
   logger.info('Shutting down gracefully...');
 
   // ...
   process.exit(0);
 
-  setTimeout(() => {
-    logger.error('Forcing shutdown.');
-    process.exit(1);
-  }, 60000);
 }
 
 process.on('SIGINT', shutdown);
