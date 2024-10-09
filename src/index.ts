f05f016592688d9bb3bc5d54518437b7d27ac2c3
alice <aliceisjustplaying@gmail.com>
2024-10-09 17:27:05 +0200

    remove shutdown timeout

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,6 @@ dotenv.config();
 logger.info('Hello, World!');
 
 function shutdown() {
-  setTimeout(() => {
-    logger.error('Forcing shutdown.');
-    process.exit(1);
-  }, 60000);
-
-
   logger.info('Shutting down gracefully...');
 
   // ...
